514a65808cff62de49349cfb2f47650b4cfc734f
kappappa-cpu <world.is.flat.kk@gmail.com>
2025-04-10 18:40:57 +0900

    Create C_ShortestDuplicateSubarray.py
    https://atcoder.jp/contests/abc395/tasks/abc395_c

---
+++ C_ShortestDuplicateSubarray.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+A = tuple([int(i) for i in input().split()])
+
+if len(A) - len(set(A)) <= 0 or len(A) == 1:
+  print(-1)
+  exit()
+
+checkDic = {}
+# key:番号, value:[出現回数,前回の座標]
+
+nears = []
+
+for i in range(N):
+  a = A[i]
+  try:
+    checkDic[a][0] += 1
+    nears.append(i - checkDic[a][1])
+    checkDic[a][1] = i #現在の座標に更新
+
+  except:
+    checkDic[a] = [1,i]
+
+print(min(nears)+1)
