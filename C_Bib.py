4e8a3640bb21e3ff5970df15876052367d3d8b09
kappappa-cpu <world.is.flat.kk@gmail.com>
2025-04-10 18:37:28 +0900

    Create C_Bib.py

---
+++ C_Bib.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+plist = [int(i) for i in input().split()]
+qlist = [int(i) for i in input().split()]
+
+corres = dict()
+
+for p, q in zip(plist,qlist):
+  corres[q] = p
+  
+answer = []
+
+for i in range(n):
+  answer.append(str(qlist[corres[i+1]-1]))
+  
+print(" ".join(answer))
