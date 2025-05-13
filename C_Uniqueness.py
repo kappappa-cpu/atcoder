8b4ce8d014864ed45380f4d75505093cb6d886ce
kappappa-cpu <world.is.flat.kk@gmail.com>
2025-05-13 17:44:37 +0900

    C_Uniqueness

---
+++ C_Uniqueness.py
@@ -0,0 +1,29 @@
+# https://atcoder.jp/contests/abc398/tasks/abc398_c
+# C - Uniquenes
+
+n = int(input())
+
+numbers = [int(i) for i in input().split()]
+
+# 持っている数: [何番目にあったか, 何個あったか]
+dic = {}
+
+for i in range(n):
+  try:
+    dic[numbers[i]][1] += 1
+  except:
+    dic[numbers[i]] = [i, 1]
+  
+    
+temp = 1
+ans = -1
+
+
+for i, j in dic.items():
+  if j[1] == 1 and temp < i:
+    temp = i
+    ans = j[0]
+if ans == -1:
+  print(ans)
+else:
+  print(ans+1)
