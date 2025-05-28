a3f5cba2a46cac32001032f4c199db6045e86d93
kappappa-cpu <world.is.flat.kk@gmail.com>
2025-05-28 18:45:34 +0900

    C - Make it Simple

---
+++ C_MakeItSimple.py
@@ -0,0 +1,31 @@
+# https://atcoder.jp/contests/abc393/tasks/abc393_c
+# C - Make it Simple
+
+n, m = map(int,input().split())
+
+eSet = set()
+
+"""
+frozensetバージョン
+
+for _ in range(m):
+  _Set = set(map(int,input().split()))
+  if len(_Set) != 1:
+    # 通常のsetは可変（mutable）でハッシュ不可能（unhashable）
+    # なのでsetのsetを作れない
+    # frozensetは不変（immutable）でハッシュ可能（hassable）
+    # なのでfrozensetを持ったsetを作る
+    eSet.add(frozenset(_Set))
+print(m-len(eSet))
+"""
+
+for _ in range(m):
+  u, v = map(int,input().split())
+  if u == v:
+    continue
+  elif u > v:
+    _tuple = (v,u)
+  else:
+    _tuple = (u, v)
+  eSet.add(_tuple)
+print(m-len(eSet))
